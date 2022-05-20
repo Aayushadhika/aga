afd405106430eac706f0bf403a627df6decf0fd3
Aayushadhika <nu6281806@gmail.com>
2022-05-20 10:49:43 +0545

    init

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+# result= lambda n1,n2,n3,:n1*n2*n3
+# print("sum of three value : ",result (10,20,30))
+
+# li=[5,7,22,97,54,62,77,23,73,61]
+# square list = list (map(lambda x: x**2,11))
+#
+# print(squre_list)
+
+# a=[1,2,3]
+# b=[3,4,5]
+# abc=list(map(lambda x,y:x+y,a,b))
+# print(abc)
+
+# data=[1,2,3,4,5,5,6,6,7,9,10]
+# var=list(filter(lambda x : x%2==0,data))
+#
+# print(var)
+
+
+
+# from functools import reduce
+# li=[5,8,10,20,50,100]
+# sum=reduce((lambda x,y:x+y),li)
+# print(sum)
